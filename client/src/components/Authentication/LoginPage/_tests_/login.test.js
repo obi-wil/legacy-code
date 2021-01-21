79305791ae5dd1b2a0d99785935ac28603bc638f
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-01-21 18:42:23 +0100

    feat: add login tests

--- client/src/components/Authentication/LoginPage/_tests_/login.test.js
+++ client/src/components/Authentication/LoginPage/_tests_/login.test.js
@@ -1,6 +1,19 @@
 import React from "react";
-import { render, fireEvent, waitFor, screen } from '@testing-library/react';
-import LoginPage from "./LoginPage.js";
+import {render, fireEvent, screen} from "../../../../utils/test-utils";
+import LoginPage from "../LoginPage";
+
+// afterEach(cleanup);
 
 
+test('should render the login form', () => {
+  const {container} = render(<LoginPage />);
+  const form = container.querySelector("form");
+  const {personname, password} = form.elements;
+
+  personname.value = "Maestro Yoda"
+  password.value = "Maestro Yoda"
+
+  const submit = new window.Event("submit");
+  form.dispatchEvent(submit)
+})
 
