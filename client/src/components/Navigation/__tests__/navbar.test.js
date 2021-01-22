62c0e1cdea69ce0b1df03d4574b18ddb2d24776e
Wilfredo Campos <wilfredo.campos@gmail.com>
2021-01-22 16:13:41 +0100

    fix: add alt properties

---
+++ client/src/components/Navigation/__tests__/navbar.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { fireEvent, reduxRender, screen } from '../../../../utils/test-utils';
+import NavBar from '../NavBar/NavBar';
+
+// TODO: assert role teacher renders vader image
+
+// TODO: assert role student renders woman image
+
+// TODO: assert if role teacher renders greeting text
+
+// TODO: assert if role teacher renders teacher links
+
+// TODO: assert if role student renders student greeting
+
+// TODO: assert if role student renders student links
