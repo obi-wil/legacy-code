d0a95f3850701ad41e8b5b4f5d7187396a536d78
Wilfredo Campos <wilfredo.campos@gmail.com>
2021-01-22 17:01:48 +0100

    feat: add teacher avatar test

--- client/src/components/Navigation/__tests__/navbar.test.js
+++ client/src/components/Navigation/__tests__/navbar.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { fireEvent, reduxRender, screen } from '../../../../utils/test-utils';
 import NavBar from '../NavBar/NavBar';
+import NavItems from '../NavItems/NavItems';
 
 // TODO: assert role teacher renders vader image
+describe('NavBar', () => {
+  it('should render teacher avatar', () => {
+    reduxRender(<NavBar />);
+    const teacherAvatar = screen.getByAltText('teacher avatar');
+
+    expect(teacherAvatar).toBeInTheDocument();
+  });
+});
 
 // TODO: assert role student renders woman image
 
