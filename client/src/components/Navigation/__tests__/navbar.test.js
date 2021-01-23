a132d0173d7a76fd5c2b6f47d0cff5b71ac46e8e
Wilfredo Campos <wilfredo.campos@gmail.com>
2021-01-23 17:17:31 +0100

    feat: add teacher role tests

--- client/src/components/Navigation/__tests__/navbar.test.js
+++ client/src/components/Navigation/__tests__/navbar.test.js
@@ -1,23 +1,48 @@
 import React from 'react';
-import { fireEvent, reduxRender, screen } from '../../../../utils/test-utils';
+import { BrowserRouter } from 'react-router-dom';
+import { reduxRender, screen } from '../../../utils/test-utils';
 import NavBar from '../NavBar/NavBar';
 import NavItems from '../NavItems/NavItems';
 
 // TODO: assert role teacher renders vader image
-describe('NavBar', () => {
-  it('should render teacher avatar', () => {
-    reduxRender(<NavBar />);
-    const teacherAvatar = screen.getByAltText('teacher avatar');
-
-    expect(teacherAvatar).toBeInTheDocument();
-  });
-});
 
 // TODO: assert role student renders woman image
 
-// TODO: assert if role teacher renders greeting text
+describe('Teacher role', () => {
+  it('should render teacher links', () => {
+    reduxRender(
+      <BrowserRouter>
+        <NavItems />
+      </BrowserRouter>,
+    );
+    const tests = screen.getByText(/my tests/i);
+    const testCreator = screen.getByText(/test creator/i);
+    const students = screen.getByText(/students/i);
+    expect(tests).toBeInTheDocument();
+    expect(testCreator).toBeInTheDocument();
+    expect(students).toBeInTheDocument();
+  });
 
-// TODO: assert if role teacher renders teacher links
+  it('should render teacher greeting', () => {
+    reduxRender(
+      <BrowserRouter>
+        <NavBar role="teacher" />
+      </BrowserRouter>,
+    );
+    const teacherGreeting = screen.getByText(/hello, mr. /i);
+    expect(teacherGreeting).toBeInTheDocument();
+  });
+
+  it('should render teacher avatar', () => {
+    reduxRender(
+      <BrowserRouter>
+        <NavBar role="teacher" />
+      </BrowserRouter>,
+    );
+    const teacherAvatar = screen.getByAltText(/teacher avatar/i);
+    expect(teacherAvatar).toBeInTheDocument();
+  });
+});
 
 // TODO: assert if role student renders student greeting
 
