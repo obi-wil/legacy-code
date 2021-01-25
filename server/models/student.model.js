d35e23651d4a8af540d42f83d6bcdde15d7b3801
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-01-25 12:34:04 +0100

    pulled changes from main

--- server/models/student.model.js
+++ server/models/student.model.js
@@ -3,28 +3,34 @@ const mongoose = require('mongoose');
 const db = require('../db');
 
 const TestSchema = mongoose.Schema({
-  name: {type: String, required: true},
-  pendingtests: {type: [], required: true, default: []},
-  completedtests: {type: [{
-    id: String,
-    title: String,
-    result: {
-      percentage: Number,
-      questions: [{
-        question: String,
+  name: { type: String, required: true },
+  pendingtests: { type: [], required: true, default: [] },
+  completedtests: {
+    type: [
+      {
         id: String,
-        option: String,
-        correct: Boolean
-      }]
-    }
-  }], default: []},
-  classes: {type: [String], required: true, default: []}
+        title: String,
+        result: {
+          percentage: Number,
+          questions: [
+            {
+              question: String,
+              id: String,
+              option: String,
+              correct: Boolean,
+            },
+          ],
+        },
+      },
+    ],
+    default: [],
+  },
+  classes: { type: [String], required: true, default: [] },
 });
 
 const student = db.conn.model('student', TestSchema);
 
 module.exports = student;
-
 
 // {
 //   "name":"Sara Gómez",
@@ -79,8 +85,7 @@ module.exports = student;
 //           ]
 //           }
 //         ,
-        
-      
+
 //       }
 //     ]
 // }
